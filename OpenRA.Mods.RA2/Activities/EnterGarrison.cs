726967dbf3496b046f69417fc1dfa8aa4ea38115
wolfbyte <devon.dieffenbach@gmail.com>
2019-02-09 14:38:23 -0500

    Fix issue with having to re-issue order.

--- OpenRA.Mods.RA2/Activities/EnterGarrison.cs
+++ OpenRA.Mods.RA2/Activities/EnterGarrison.cs
@@ -12,36 +12,57 @@
 using System;
 using System.Linq;
 using OpenRA.Mods.Common.Traits;
-using OpenRA.Traits;
+using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Activities;
+using OpenRA.Traits;
 using OpenRA.Mods.RA2.Traits;
 using System.Diagnostics;
+using System.Drawing;
+using OpenRA.Activities;
 namespace OpenRA.Mods.RA2.Activities
 {
-    class EnterGarrison : Enter
+    class EnterGarrison : Activity
     {
+        // Base Enter Properties 
+        enum EnterState { Approaching, Waiting, Entering, Exiting }
+        readonly IMove move;
+        readonly Color? targetLineColor;
+        Target target;
+        Target lastVisibleTarget;
+        bool useLastVisibleTarget;
+        EnterState lastState = EnterState.Approaching;
+        Activity moveActivity;
+
+        // EnterGarrison Properties
         readonly Garrisoner garrisoner;
         Target garrisonableBuilding;
         Actor enterActor;
         Garrison enterGarrison;
 
+
+
         public EnterGarrison(Actor self, Target garrisonableBuilding)
-            : base(self, garrisonableBuilding)
         {
+            // Base - Enter Properties 
+            move = self.Trait<IMove>();
+            this.target = garrisonableBuilding;
+
+            // EnterGarrison Properties
+            this.targetLineColor = null;
             this.garrisonableBuilding = garrisonableBuilding;
             garrisoner = self.TraitsImplementing<Garrisoner>().Single();
         }
 
         // self == the garrisoner (e1)
         // TargetActor == garrisonable building CaWash
-        protected override bool TryStartEnter(Actor self, Actor targetActor)
+        protected bool TryStartEnter(Actor self, Actor targetActor)
         {
             enterActor = targetActor;
             enterGarrison = targetActor.TraitOrDefault<Garrison>();
 
             // Make sure we can still enter the transport
             // (but not before, because this may stop the actor in the middle of nowhere)
-            if (enterGarrison== null || !garrisoner.Reserve(self, enterGarrison))
+            if (enterGarrison == null || !garrisoner.Reserve(self, enterGarrison))
             {
                 Cancel(self, true);
                 return false;
@@ -49,10 +70,11 @@ namespace OpenRA.Mods.RA2.Activities
 
             return true;
         }
-        protected override void OnCancel(Actor self) {
-            Debug.WriteLine("Fuck");
+        protected void OnCancel(Actor self)
+        {
+            Debug.WriteLine("Cancelled");
         }
-        protected override void OnEnterComplete(Actor self, Actor targetActor)
+        protected void OnEnterComplete(Actor self, Actor targetActor)
         {
             self.World.AddFrameEndTask(w =>
             {
@@ -71,6 +93,133 @@ namespace OpenRA.Mods.RA2.Activities
                 // EnterTransports corrupt the actor state. Activities are cancelled again on unload
                 self.CancelActivity();
             });
+        }
+
+        /// Base Enter Methods Below
+        public override Activity Tick(Actor self)
+        {
+            // Update our view of the target
+            bool targetIsHiddenActor;
+            target = target.Recalculate(self.Owner, out targetIsHiddenActor);
+
+            //Re-acquire the target after change owner has happened. 
+            if (target.Type == TargetType.Invalid)
+            {
+                target = Target.FromActor(target.Actor);
+            }
+
+            if (!targetIsHiddenActor && target.Type == TargetType.Actor)
+                lastVisibleTarget = Target.FromTargetPositions(target);
+
+            var oldUseLastVisibleTarget = useLastVisibleTarget;
+            useLastVisibleTarget = targetIsHiddenActor || !target.IsValidFor(self);
+
+            // Cancel immediately if the target died while we were entering it
+            if (!IsCanceled && useLastVisibleTarget && lastState == EnterState.Entering)
+                Cancel(self, true);
+
+            //TickInner(self, target, useLastVisibleTarget); // Left Over from Enter. unused in EnterGarrison
+
+            // Update target lines if required
+            if (useLastVisibleTarget != oldUseLastVisibleTarget && targetLineColor.HasValue)
+                self.SetTargetLine(useLastVisibleTarget ? lastVisibleTarget : target, targetLineColor.Value, false);
+
+            // We need to wait for movement to finish before transitioning to
+            // the next state or next activity
+            if (moveActivity != null)
+            {
+                moveActivity = ActivityUtils.RunActivity(self, moveActivity);
+                if (moveActivity != null)
+                    return this;
+            }
+
+            // Note that lastState refers to what we have just *finished* doing
+            switch (lastState)
+            {
+                case EnterState.Approaching:
+                case EnterState.Waiting:
+                    {
+                        // NOTE: We can safely cancel in this case because we know the
+                        // actor has finished any in-progress move activities
+                        if (IsCanceled)
+                            return NextActivity;
+
+                        // Lost track of the target
+                        if (useLastVisibleTarget && lastVisibleTarget.Type == TargetType.Invalid)
+                            return NextActivity;
+
+                        // We are not next to the target - lets fix that
+                        if (target.Type != TargetType.Invalid && !move.CanEnterTargetNow(self, target))
+                        {
+                            lastState = EnterState.Approaching;
+
+                            // Target lines are managed by this trait, so we do not pass targetLineColor
+                            var initialTargetPosition = (useLastVisibleTarget ? lastVisibleTarget : target).CenterPosition;
+                            moveActivity = ActivityUtils.RunActivity(self, move.MoveToTarget(self, target, initialTargetPosition));
+                            break;
+                        }
+
+                        // We are next to where we thought the target should be, but it isn't here
+                        // There's not much more we can do here
+                        if (useLastVisibleTarget || target.Type != TargetType.Actor)
+                            return NextActivity;
+
+                        // Are we ready to move into the target?
+                        if (TryStartEnter(self, target.Actor))
+                        {
+                            lastState = EnterState.Entering;
+                            moveActivity = ActivityUtils.RunActivity(self, move.MoveIntoTarget(self, target));
+                            return this;
+                        }
+
+                        // Subclasses can cancel the activity during TryStartEnter
+                        // Return immediately to avoid an extra tick's delay
+                        if (IsCanceled)
+                            return NextActivity;
+
+                        lastState = EnterState.Waiting;
+                        break;
+                    }
+
+                case EnterState.Entering:
+                    {
+                        
+                        if (target.Type == TargetType.Invalid)
+                        {
+                            target = Target.FromActor(target.Actor);
+                        }
+                        // Check that we reached the requested position
+                        var targetPos = target.Positions.PositionClosestTo(self.CenterPosition);
+
+                        if (!IsCanceled && self.CenterPosition == targetPos && target.Type == TargetType.Actor)
+                        {  
+                            OnEnterComplete(self, target.Actor);
+                        }
+                        else
+                        { // Need to move again as we have re-aquired a target
+                            moveActivity = ActivityUtils.RunActivity(self, move.MoveToTarget(self, target, targetPos));
+                            lastState = EnterState.Approaching;
+                            break;
+                        }
+                        lastState = EnterState.Exiting;
+                        moveActivity = ActivityUtils.RunActivity(self, move.MoveIntoWorld(self, self.Location));
+                        break;
+                    }
+
+                case EnterState.Exiting:
+                    return NextActivity;
+            }
+
+            return this;
+        }
+        public override bool Cancel(Actor self, bool keepQueue = false)
+        {
+            OnCancel(self);
+
+            if (!IsCanceled && moveActivity != null && !moveActivity.Cancel(self))
+                return false;
+
+            return base.Cancel(self, keepQueue);
         }
 
     }
