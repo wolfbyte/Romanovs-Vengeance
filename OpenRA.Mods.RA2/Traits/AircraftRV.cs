733ec30f12c69fce837d2bd6271551e1e05d7972
wolfbyte <devon.dieffenbach@gmail.com>
2019-03-09 13:00:45 -0500

    Added AircraftRV Trait

---
+++ OpenRA.Mods.RA2/Traits/AircraftRV.cs
@@ -0,0 +1,54 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using OpenRA.Activities;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+
+namespace OpenRA.Mods.RA2.Traits
+{
+	public class AircraftG5Info : AircraftInfo, ITraitInfo, IPositionableInfo, IFacingInfo, IMoveInfo, ICruiseAltitudeInfo,
+	IActorPreviewInitInfo, IEditorActorOptions
+	{
+		new public virtual object Create(ActorInitializer init) { return new AircraftG5(init, this); }
+	}
+
+	public class AircraftG5 : Aircraft, ITick, ISync, IFacing, IPositionable, IMove, IIssueOrder, IResolveOrder, IOrderVoice, IDeathActorInitModifier,
+	INotifyCreated, INotifyAddedToWorld, INotifyRemovedFromWorld, INotifyActorDisposing, INotifyBecomingIdle,
+	IActorPreviewInitModifier, IIssueDeployOrder, IObservesVariables
+	{
+		new public readonly AircraftG5Info Info;
+		readonly Actor self;
+
+		public AircraftG5(ActorInitializer init, AircraftInfo info) : base(init, info)
+		{
+			Info = (AircraftG5Info)info;
+			self = init.Self;
+
+			if (init.Contains<LocationInit>())
+				SetPosition(self, init.Get<LocationInit, CPos>());
+
+			if (init.Contains<CenterPositionInit>())
+				SetPosition(self, init.Get<CenterPositionInit, WPos>());
+
+			Facing = init.Contains<FacingInit>() ? init.Get<FacingInit, int>() : Info.InitialFacing;
+		}
+	}
+
+}
